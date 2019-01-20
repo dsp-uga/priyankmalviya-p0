dd1f6bb77844527a839a1f315b0a0121af7e9d75
Priyank Malviya <42504485+priyankmalviya@users.noreply.github.com>
2019-01-20 04:51:02 -0500

    Add files via upload

--- P0.py
+++ P0.py
@@ -24,48 +24,161 @@ conf = (conf.setMaster('local[*]'))
 sc = pyspark.SparkContext(conf=conf)
 
 
-# ### read the file
-# 
+# ## Reading the file
 
-# In[5]:
+# In[4]:
 
 
 file = sc.textFile('/home/mr_malviya/Desktop/Secret/Study/Spring_2019/DSP/P0/handout/data')
 
 
-# ### finding the word counts and writing the output as json file
-
-# In[9]:
+# In[5]:
 
 
-rawcounts = file.flatMap(lambda x : x.split(' ')).map(lambda word : (word,1)).reduceByKey(lambda a,b : a+b)
+file.count()
 
 
-# In[10]:
+# ## Finding the word count and writing the output as json
+
+# In[49]:
+
+
+rawcounts = file.flatMap(lambda x : x.split(' ')).map(lambda x : x.lower()).map(lambda word : (word,1)).reduceByKey(lambda a,b : a+b)
+
+
+# In[50]:
 
 
 rawcounts = rawcounts.collectAsMap()
 
 
-# In[11]:
+# In[51]:
 
 
 rawcounts = json.dumps(rawcounts)
 
 
-# In[12]:
+# In[52]:
 
 
 f = open('/home/mr_malviya/Desktop/rawcounts.json','w')
 
 
-# In[13]:
+# In[53]:
 
 
 f.write(str(rawcounts))
 
 
-# In[14]:
+# In[54]:
+
+
+f.close()
+
+
+# ## Find the top 40 words across all documents
+
+# In[60]:
+
+
+spA = file.flatMap(lambda x : x.split(' ')).map(lambda x : x.lower()).map(lambda x : (x,1)).reduceByKey(lambda a,b : a + b)
+
+
+# In[61]:
+
+
+spA = spA.filter(lambda x : x[1] > 1)
+spA.take(5)
+
+
+# In[62]:
+
+
+spA = spA.sortBy(lambda a : a[1], False)
+
+
+# In[63]:
+
+
+spA = spA.top(40, key = lambda x : x[1])
+
+
+# In[64]:
+
+
+spA = json.dumps(spA)
+
+
+# In[65]:
+
+
+f = open('/home/mr_malviya/Desktop/spA.json','w')
+
+
+# In[66]:
+
+
+f.write(str(spA))
+
+
+# In[67]:
+
+
+f.close()
+
+
+# ### stopwords
+
+# In[32]:
+
+
+stopwords = sc.textFile('/home/mr_malviya/Desktop/Secret/Study/Spring_2019/DSP/P0/handout/stopwords.txt')
+stopwords = stopwords.collect()
+
+
+# In[21]:
+
+
+file = sc.textFile('/home/mr_malviya/Desktop/Secret/Study/Spring_2019/DSP/P0/handout/data')
+
+
+# In[45]:
+
+
+spB = file.flatMap(lambda x : x.split(' ')).filter(lambda x : x.lower() not in stopwords).map(lambda x : (x,1)).reduceByKey(lambda a,b : a+b)
+
+
+# In[46]:
+
+
+spB = spB.sortBy(lambda a : a[1], False)
+
+
+# In[47]:
+
+
+spB = spB.top(40, key = lambda x : x[1])
+
+
+# In[55]:
+
+
+spB = json.dumps(spB)
+
+
+# In[57]:
+
+
+f = open('/home/mr_malviya/Desktop/sp2.json','w')
+
+
+# In[58]:
+
+
+f.write(str(spB))
+
+
+# In[59]:
 
 
 f.close()
